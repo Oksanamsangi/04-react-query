81d8c59fde1ff73b1a8f0d891e7ede4ee5a8760d
Oksanamsangi <162930811+Oksanamsangi@users.noreply.github.com>
2025-07-17 01:13:45 -0600

    first commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,76 @@
+import { useState, useEffect } from 'react';
+import { useQuery, keepPreviousData } from '@tanstack/react-query';
+import toast from 'react-hot-toast';
+import ReactPaginate from 'react-paginate';
+import type { Movie } from '../../types/movie';
+import { fetchMovies } from '../../services/movieService';
+import SearchBar from '../SearchBar/SearchBar';
+import MovieGrid from '../MovieGrid/MovieGrid';
+import Loader from '../Loader/Loader';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import MovieModal from '../MovieModal/MovieModal';
+import css from '../App/App.module.css';
+
+export default function App() {
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+
+  const { data, isPending, isError, isSuccess } = useQuery({
+    queryKey: ['movie', query, page],
+    queryFn: () => fetchMovies(query, page),
+    enabled: !!query,
+    placeholderData: keepPreviousData,
+  });
+
+  useEffect(() => {
+    if (query && data && data.results.length === 0) {
+      toast('No movies found for your request.');
+    }
+  }, [data, query]);
+
+  const handleSubmit = (newQuery: string) => {
+    setQuery(newQuery);
+    setPage(1);
+  };
+
+  return (
+    <>
+      <SearchBar onSubmit={handleSubmit} />
+
+      {}
+      {query && isPending && !data && <Loader />}
+
+      {isError && <ErrorMessage />}
+
+      {isSuccess && data.results.length > 0 && (
+        <>
+          {data.total_pages > 1 && (
+            <ReactPaginate
+              pageCount={data.total_pages}
+              pageRangeDisplayed={5}
+              marginPagesDisplayed={1}
+              onPageChange={({ selected }) => setPage(selected + 1)}
+              forcePage={page - 1}
+              containerClassName={css.pagination}
+              activeClassName={css.active}
+              nextLabel="→"
+              previousLabel="←"
+            />
+          )}
+          <MovieGrid
+            movies={data.results}
+            onSelect={(movie: Movie) => setSelectedMovie(movie)}
+          />
+        </>
+      )}
+
+      {selectedMovie && (
+        <MovieModal
+          movie={selectedMovie}
+          onClose={() => setSelectedMovie(null)}
+        />
+      )}
+    </>
+  );
+}
